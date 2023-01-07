f0c67e33bd0326331fe11d8c561290ab45941b21
Jeremy Kuzub <jeremy@jufaintermedia.com>
2023-01-06 23:11:21 -0400

    update colors - E755 white - no more color 86!

--- runinventory.mjs
+++ runinventory.mjs
@@ -291,8 +291,8 @@ inv = pullFromInventory(e755Ora,inv);
 inv = pullFromInventory(e755WhtBlu,inv);
 
 // 15 Nov
-inv = pullFromInventory(EX761KeithBlackPurpleBlue,inv);
-inv = pullFromInventory(EX761KeithWhitePurpleBlue,inv);
+// inv = pullFromInventory(EX761KeithBlackPurpleBlue,inv); //seems these orders were lost during the move in terms of xml? omit
+// inv = pullFromInventory(EX761KeithWhitePurpleBlue,inv);
 
 // 20 nov
 inv = pullFromInventory(e755Azu,inv);
@@ -345,15 +345,15 @@ inv = pullFromInventory(e755Ora,inv);
 // stock
 //
 
-inv = pullFromInventory(e755WhtBlu, inv, 0);
-inv = pullFromInventory(e755WhtOra, inv, 0);
+inv = pullFromInventory(e755WhtBlu, inv, 3);
+inv = pullFromInventory(e755WhtOra, inv, 1);
 inv = pullFromInventory(e755Azu, inv, 0);
 inv = pullFromInventory(e755Ora, inv, 0);
 
-inv = pullFromInventory(EX763ChromeLightBlue20221219, inv, 0);
-inv = pullFromInventory(EX763ChromeNeonGreen20221219, inv, 0);
-inv = pullFromInventory(RecognizerTransLightBlue20221219, inv, 0);
-inv = pullFromInventory(RecognizerTransOrange20221219, inv, 0);
+// inv = pullFromInventory(EX763ChromeLightBlue20221219, inv, 1);
+// inv = pullFromInventory(EX763ChromeNeonGreen20221219, inv, 1);
+// inv = pullFromInventory(RecognizerTransLightBlue20221219, inv, 1);
+// inv = pullFromInventory(RecognizerTransOrange20221219, inv, 1);
 
 //
 // Damaged, lost, substandard
